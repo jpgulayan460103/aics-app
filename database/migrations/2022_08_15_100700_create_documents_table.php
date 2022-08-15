5bcf3d0736c50175584e083177c5516238ab22f3
John Paul Gulayan <jpgulayan@gmail.com>
2022-08-15 11:30:15 +0800

    add document

--- database/migrations/2022_08_15_100700_create_documents_table.php
+++ database/migrations/2022_08_15_100700_create_documents_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDocumentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('documents', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('uuid');
-            $table->rememberToken();
+            $table->string('document_type')->nullable();
+            $table->string('file_directory')->nullable();
+            $table->morphs('clientable');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('documents');
     }
 }
