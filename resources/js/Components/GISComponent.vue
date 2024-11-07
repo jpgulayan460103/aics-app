23043ba9d029767f979ca32189075344342d48ab
Mark Rezzel Sarabia <sarabiamark0012@gmail.com>
2024-11-07 12:00:48 +0800

    fix giscomponent

--- resources/js/Components/GISComponent.vue
+++ resources/js/Components/GISComponent.vue
@@ -283,16 +283,10 @@
                 <div class="col-md-3">
                   <label for="birth_date">Kapanganakan <small>(Birthdate)</small><span
                       style="color:red">*</span></label>
-                  <div v-if="userData.role != 'Super-Admin'">
-                    <v-text-field type="date" @input="calculateAge" v-model="form.birth_date" required
-                      :error-messages="validationErrors.birth_date" outlined dense readonly>
-                    </v-text-field>
-                  </div>
-                  <div v-else>
-                    <v-text-field type="date" @input="calculateAge" v-model="form.birth_date" required
-                      :error-messages="validationErrors.birth_date" outlined dense>
-                    </v-text-field>
-                  </div>
+
+                  <v-text-field type="date" @input="calculateAge" v-model="form.birth_date" required
+                    :error-messages="validationErrors.birth_date" outlined dense :readonly="userData.role != 'Super-Admin'">
+                  </v-text-field>
 
                 </div>
 
@@ -580,6 +574,7 @@
 
 <script>
 import { debounce, cloneDeep } from 'lodash'
+import { readonly } from 'vue';
 
 export default {
   props: ["dialog_data", "getList", "userData", "setDialogCreate", "provinces"],
